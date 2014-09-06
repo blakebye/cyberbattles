41874c893d66fe1450cd261a86e8f84997d5be64
Blake Bye <blake.bye@gmail.com>
2014-09-05 19:05:31 -0700

    give structures alignments, still string for now

--- structures.py
+++ structures.py
@@ -5,15 +5,23 @@ class Structure(object):
 class Fortress(Structure):
     def __init__(self):
         self.name = "Fortress"
+        self.probability = 0.6
+        self.alignment = 'T'
 
 class GunTurret(Structure):
     def __init__(self):
         self.name = "Gun Turret"
+        self.probability = 0.5
+        self.alignment = 'T'
 
 class SubspaceBeacon(Structure):
     def __init__(self):
         self.name = "SubspaceBeacon"
+        self.probability = 0.9
+        self.alignment = 'T'
 
 class ForceField(Structure):
     def __init__(self):
         self.name = "Force Field"
+        self.probability = 0.9
+        self.alignment = 'T'

835988eb1e28e37af1b024aefc8205aa2904642a
Blake Bye <blake.bye@gmail.com>
2014-09-05 20:25:51 -0700

    gives structures integer alignments

--- structures.py
+++ structures.py
@@ -6,22 +6,22 @@ class Fortress(Structure):
     def __init__(self):
         self.name = "Fortress"
         self.probability = 0.6
-        self.alignment = 'T'
+        self.alignment = 1
 
 class GunTurret(Structure):
     def __init__(self):
         self.name = "Gun Turret"
         self.probability = 0.5
-        self.alignment = 'T'
+        self.alignment = 1
 
 class SubspaceBeacon(Structure):
     def __init__(self):
         self.name = "SubspaceBeacon"
         self.probability = 0.9
-        self.alignment = 'T'
+        self.alignment = 1
 
 class ForceField(Structure):
     def __init__(self):
         self.name = "Force Field"
         self.probability = 0.9
-        self.alignment = 'T'
+        self.alignment = 1

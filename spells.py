be9a37e7840b8e4ac58dcbb78bd2af48173876d5
Blake Bye <blake.bye@gmail.com>
2014-09-05 20:26:01 -0700

    gives spells integer alignments

---
+++ spells.py
@@ -0,0 +1,60 @@
+class Spell(object):
+    def __init__(self):
+        pass
+
+class HoloDetect(Spell):
+    def __init__(self):
+        self.name = "HoloDetect"
+        self.probability = 1.00
+        self.alignment = 0
+
+class Mutate(Spell):
+    def __init__(self):
+        self.name = "Mutate"
+        self.probability = 0.7
+        self.alignment = -1
+
+class Hypnotize(Spell):
+    def __init__(self):
+        self.name = "Hypnotize"
+        self.probability = 1.00
+        # DEPENDS ON RESISTANCE
+        self.alignment = 0
+
+class Resurrect(Spell):
+    def __init__(self):
+        self.name = "Resurrect"
+        self.probability = 0.6
+        self.alignment = -1
+
+class Disrupt(Spell):
+    def __init__(self):
+        self.name = "Disrupt"
+        self.probability = 1.00
+        # DEPENDS ON RESISTANCE AND DEFENSE
+        self.alignment = 0
+
+class Disintegrate(Spell):
+    def __init__(self):
+        self.name = "Disintegrate"
+        self.probability = 1.00
+        # DEPENDS ON RESISTANCE AND DEFENSE
+        self.alignment = 0
+
+class EMP(Spell):
+    def __init__(self):
+        self.name = "EMP"
+        self.probability = 0.5
+        self.alignment = 1
+
+class Virus(Spell):
+    def __init__(self):
+        self.name = "Virus"
+        self.probability = 0.5
+        self.alignment = -1
+
+class Teleport(Spell):
+    def __init__(self):
+        self.name = "Teleport"
+        self.probability = 0.7
+        self.alignment = 0

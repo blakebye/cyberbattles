0320696806417d77a6e0c14f5529507a8404f1f7
Blake Bye <blake.bye@gmail.com>
2014-09-05 22:36:05 -0700

    add cast_spell for emp/virus/align/resurrect

--- board.py
+++ board.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import random
 import creatures as c
 import structures as s
+import spells as sp
 
 class Gameboard(object):
     def __init__(self, width, height, number_of_players):
@@ -18,6 +19,13 @@ class Gameboard(object):
         message = ""
 
     def print_board(self):
+        if self.alignment == 0:
+            align = "<none>"
+        elif self.alignment > 0:
+            align = "Technology %i" % abs(self.alignment)
+        elif self.alignment < 0:
+            align = "Lifeforce %i" % abs(self.alignment)
+        print("ALIGNMENT: %s" % align)
         print(' ', sep='', end='')
         print('-' * (self.width * 2), sep='')
         for row in range(len(self.board)):
@@ -30,9 +38,11 @@ class Gameboard(object):
                     list_of_elements.append(element)
             for square in list_of_elements:
                 if square["alive"] != 0:
-                    print ("{}".format(square["alive"].name[0]), sep='', end=' ')
+                    print ("{}".format(square["alive"].name[0]), 
+                           sep='', end=' ')
                 elif square["dead"] != 0:
-                    print ("{}".format(square["dead"].name[0].lower()), sep='', end=' ')
+                    print ("{}".format(square["dead"].name[0].lower()), 
+                           sep='', end=' ')
                 else:
                     print ("{}".format(" "), sep='', end=' ')
             print ('|')
@@ -42,7 +52,6 @@ class Gameboard(object):
     def spawn_commanders(self):
         if number_of_players == 1:
             pass
-
 
     def beam_creature(self, x, y, creature):
         if self.board[self.height - y][x - 1]["alive"] == 0:
@@ -63,6 +72,7 @@ class Gameboard(object):
         if isinstance(structure, s.Fortress):
             if self.board[self.height - y][x - 1]["alive"] == 0:
                 self.board[self.height - y][x - 1]["alive"] = structure
+
         elif (isinstance(structure, s.GunTurret) or 
               isinstance(structure, s.SubspaceBeacon)):
             for i, j in ((x - 1, y), (x + 1, y), (x, y + 2), (x, y - 2), 
@@ -72,6 +82,7 @@ class Gameboard(object):
                     j <= self.height and i <= self.width):
                     if self.board[self.height - j][i - 1]["alive"] == 0:
                         self.board[self.height - j][i - 1]["alive"] = structure
+
         elif isinstance(structure, s.ForceField):
             for i, j in ((x - 2, y + 1), (x - 2, y + 2), (x - 1, y + 2), 
                          (x + 2, y + 1), (x + 2, y + 2), (x + 1, y + 2),
@@ -90,7 +101,7 @@ class Gameboard(object):
         return self.board[self.height - y][x - 1]["dead"]
 
     def kill_creature(self, x, y):
-        if self.board[self.height - y][x - 1]["alive"].holograph == False:
+        if self.check_occupancy(x, y).holograph != True:
             self.board[self.height - y][x - 1]["dead"] = \
             self.board[self.height - y][x - 1]["alive"]
         self.board[self.height - y][x - 1]["alive"] = 0
@@ -99,7 +110,52 @@ class Gameboard(object):
         self.board[self.height - y][x - 1]["alive"] = 0
 
     def kill_commander(self, x, y, commander):
-        self.board[self.height - y][x - 1] = 0
+        self.board[self.height - y][x - 1]["alive"] = 0
+
+    def cast_spell(self, x, y, spell):
+        if isinstance(spell, sp.HoloDetect):
+            if self.board[self.height - y][x - 1]["alive"].holograph == True:
+                self.board[self.height - y][x - 1]["alive"] = 0
+            else:
+                pass
+        elif isinstance(spell, sp.Mutate):
+            # requires all creatures to be implementes as cards
+            pass
+        elif isinstance(spell, sp.Hypnotize):
+            # requires commanders be implemented
+            pass
+
+        elif isinstance(spell, sp.Resurrect):
+            self.board[self.height - y][x - 1]["alive"] = \
+            self.board[self.height - y][x - 1]["dead"]
+
+            self.board[self.height - y][x - 1]["dead"] = 0
+
+        elif isinstance(spell, sp.Disrupt):
+            # think about it for a bit
+            pass
+        elif isinstance(spell, sp.Disintegrate):
+            # think about it for a bit
+            pass
+
+        elif isinstance(spell, sp.EMP):
+            if self.check_occupancy(x, y).alignment > 0:
+                self.kill_creature(x, y)
+
+        elif isinstance(spell, sp.Virus):
+            if self.check_occupancy(x, y).alignment < 0:
+                self.kill_creature(x, y)
+
+        elif isinstance(spell, sp.Teleport):
+            # requires commander be implemented
+            pass
+
+        elif isinstance(spell, sp.AlignTech):
+            self.align_tech(spell.level)
+
+        elif isinstance(spell, sp.AlignLife):
+            self.align_life(spell.level)
+
 
     def align_life(self, level):
         # LIFE WILL BE NEGATIVE

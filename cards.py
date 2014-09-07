e0a7387082b0719aa66aef8db75ee235feb5cf23
Blake Bye <blake.bye@gmail.com>
2014-09-07 01:10:26 -0700

    spawncomms  makes x players with unique hands

--- cards.py
+++ cards.py
@@ -1,129 +1,278 @@
 import random
-import creatures
 
-class AlienHatchling(creatures.Creature):
+class Creature(object):
+    """This is the basic melee creature"""
+    def __init__(self, name, probability, strength,
+               defense, speed, resist, alignment):
+        self.name = name
+        self.probability = probability
+        self.strength = strength
+        self.defense = defense
+        self.speed = speed
+        self.resist = resist
+        self.alignment = alignment
+        self.range = 0
+        self.rstr = 0
+        self.cast_range = 1
+        self.flight = False
+        self.slimy = False
+        self.mount = False
+        self.holograph = False
+        self.x = 0
+        self.y = 0
+        self.commander = 0
+
+    def movable_squares(self):
+        movable = []
+
+class Commander(Creature):
+    """This creature is a commander."""
+    def __init__(self):
+        self.name = "Commander"
+        self.strength = 5
+        self.defense = 5
+        self.speed = 1
+        self.resist = 7
+        self.range = 1
+        self.flight = False
+        self.on_mount = False
+        self.hand = cards.create_hand()
+        self.active_creatures = []
+
+    def addCreature(creature):
+        self.active_creatures.append(creature)
+        creature.commander = self
+
+    def upgrade(self, up):
+        if up == "Symbiont":
+            self.strength = max(self.strength, 6)
+            self.defense = max(self.defense, 6)
+            self.speed = max(self.speed, 2)
+            self.resist = max(self.resist, 8)
+        if up == "Force Shield":
+            self.defense = max(self.defense, 7)
+        if up == "Force Armor":
+            self.defense = max(self.defense, 8)
+        if up == "Blaster":
+            self.range = max(self.range, 3)
+        if up == "Light Sabre":
+            self.strength = max(self.strength, 7)
+        if up == "Jetpack":
+            self.speed = max(self.speed, 3)
+            self.flight = True
+
+class RangedCreature(Creature):
+    """This creature can attack from range and melee"""
+    def __init__(self, name, probability, strength, defense,
+                 speed, resist, alignment, range, rstr):
+        # define a rangedcreature by defining a creature and then
+        # giving this creature its range and strength
+        super(RangedCreature, self).__init__(name, probability, strength,
+                                             defense, speed, resist, alignment)
+        self.range = range
+        self.rstr = rstr
+
+class FlyingCreature(Creature):
+    """This creature can move unobstructed by other creatures/structures"""
+    def __init__(self, name, probability, strength,
+                 defense, speed, resist, alignment):
+        # define a flyingcreature by definint a creature and then
+        # giving this creature the property of flight
+        super(FlyingCreature, self).__init__(name, probability, strength,
+                                            defense, speed, resist, alignment)
+        self.flight = True
+
+class SlimyCreature(Creature):
+    """This creature can't be melee attacked by non-slime creatures"""
+    def __init__(self, name, probability, strength,
+                 defense, speed, resist, alignment):
+        # define a slimycreature by defining a creature and then
+        # giving this creature the property of slime
+        super(SlimyCreature, self).__init__(name, probability, strength,
+                                            defense, speed, resist, alignment)
+        self.slimy = True
+
+class Mount(Creature):
+    """This creature can hold your commander"""
+    def __init__(self, name, probability, strength,
+                 defense, speed, resist, alignment):
+        # define a mount as a creature and then give it
+        # the properties of mount and unoccupied
+        super(Mount, self).__init__(name, probability, strength,
+                                    defense, speed, resist, alignment)
+        self.mount = True
+        self.occupied = False
+
+class FlyingMount(Mount, FlyingCreature):
+    """This creature can hold your commander and fly"""
+    def __init__(self, name, probability, strength, defense,
+                 speed, resist, alignment):
+        # define a flying mount by defining a mount and then
+        # giving it the property of flight
+        super(FlyingMount, self).__init__(name, probability, strength,
+                                          defense, speed, resist, alignment)
+        self.flight = True
+
+class RangedMount(Mount, RangedCreature):
+    """This creature can hold your commander and fly"""
+    def __init__(self, name, probability, strength, defense,
+                 speed, resist, alignment, range, rstr):
+        # define a ranged mount by defining a mount and then
+        # giving it the property of range
+        super(RangedCreature, self).__init__(name, probability, strength, 
+                                             defense, speed, resist, alignment)
+        self.range = range
+        self.rstr = rstr
+        self.mount = True
+        self.occupied = False
+
+class FlyingSlimyCreature(SlimyCreature, FlyingCreature):
+    """This creature is slimy and flying"""
+    def __init__(self, name, probability, strength, defense,
+                 speed, resist, alignment):
+        # define a flying slimy creature by first defining a slimy
+        # creature and then giving it the property of flight
+        super(FlyingSlimyCreature, self).__init__(name, probability, strength,
+                                           defense, speed, resist, alignment)
+        self.flight = True
+
+class FlyingRangedMount(Mount, FlyingCreature, RangedCreature):
+    """This creature can hold your commander, fly, and has a ranged attack"""
+    def __init__(self, name, probability, strength, defense,
+                 speed, resist, alignment, range, rstr):
+        # define a flying ranged mount by first defining it as a creature
+        # and then giving it the properties of flight, range, and mount
+        super(RangedCreature, self).__init__(name, probability, strength,
+                                           defense, speed, resist, alignment)
+        self.flight = True
+        self.range = range
+        self.rstr = rstr
+        self.mount = True
+        self.occupied = False
+
+class AlienHatchling(Creature):
     def __init__(self):
         super(AlienHatchling, self).__init__("Alien Hatchling", 
                                              0.9, 2, 3, 1, 0, -2)
 
-class Probe(creatures.FlyingCreature):
+class Probe(FlyingCreature):
     def __init__(self):
         super(Probe, self).__init__("Probe", 0.9, 1, 1, 4, 2, 1)
 
-class Droid(creatures.Creature):
+class Droid(Creature):
     def __init__(self):
         super(Droid, self).__init__("Droid", 0.8, 3, 2, 1, 3, 1)
 
-class SlimeBlob(creatures.SlimyCreature):
+class SlimeBlob(SlimyCreature):
     def __init__(self):
         super(SlimeBlob, self).__init__("Slime Blob", 0.7, 3, 2, 1, 3, -1)
 
-class LaserRobot(creatures.RangedCreature):
+class LaserRobot(RangedCreature):
     def __init__(self):
         super(LaserRobot, self).__init__("Laser Robot", 0.6, 
                                          5, 1, 2, 4, 2, 5, 4)
 
-class FloatingEye(creatures.FlyingCreature):
+class FloatingEye(FlyingCreature):
     def __init__(self):
         super(FloatingEye, self).__init__("Floating Eye", 0.5, 4, 3, 6, 3, -1)
 
-class FaceGrabber(creatures.Creature):
+class FaceGrabber(Creature):
     def __init__(self):
         super(FaceGrabber, self).__init__("Face Grabber", 0.8, 2, 2, 3, 4, -2)
 
-class Jeep(creatures.Mount):
+class Jeep(Mount):
     def __init__(self):
         super(Jeep, self).__init__("Jeep", 0.9, 2, 2, 4, 4, 1)
 
-class SlimeWarrior(creatures.SlimyCreature):
+class SlimeWarrior(SlimyCreature):
     def __init__(self):
         super(SlimeWarrior, self).__init__("Slime Warrior", 0.6, 4, 2, 2, 4, -1)
 
-class Drone(creatures.Creature):
+class Drone(Creature):
     def __init__(self):
         super(Drone, self).__init__("Drone", 0.9, 1, 3, 4, 4, 1)
 
-class SentryRobot(creatures.Creature):
+class SentryRobot(Creature):
     def __init__(self):
         super(SentryRobot, self).__init__("Sentry Robot", 0.7, 3, 2, 2, 5, 1)
 
-class Alien(creatures.Creature):
+class Alien(Creature):
     def __init__(self):
         super(Alien, self).__init__("Alien", 0.7, 3, 3, 2, 5, 2)
 
-class FireFox(creatures.Mount):
+class FireFox(Mount):
     def __init__(self):
         super(FireFox, self).__init__("Fire Fox", 0.7, 1, 3, 3, 5, -1)
 
-class SlimeBeast(creatures.SlimyCreature):
+class SlimeBeast(SlimyCreature):
     def __init__(self):
         super(SlimeBeast, self).__init__("Slime Beast", 0.5, 4, 3, 3, 5, -2)
 
-class Hovership(creatures.FlyingMount):
+class Hovership(FlyingMount):
     def __init__(self):
         super(Hovership, self).__init__("Hovership", 0.6, 2, 5, 5, 4, 2)
 
-class MutantMount(creatures.Mount):
+class MutantMount(Mount):
     def __init__(self):
         super(MutantMount, self).__init__("Mutant Mount", 0.6, 3, 4, 5, 5, -2)
 
-class Cyborg(creatures.Creature):
+class Cyborg(Creature):
     def __init__(self):
         super(Cyborg, self).__init__("Cyborg", 0.5, 6, 6, 2, 4, 0)
 
-class HoverTank(creatures.FlyingRangedMount):
+class HoverTank(FlyingRangedMount):
     def __init__(self):
         super(HoverTank, self).__init__("Hover Tank", 0.4, 5, 6, 4, 4, 2, 3, 2)
 
-class MiningDroid(creatures.RangedCreature):
+class MiningDroid(RangedCreature):
     def __init__(self):
         super(MiningDroid, self).__init__("Mining Droid", 0.6, 
                                           5, 6, 1, 4, 1, 2, 2)
 
-class Tank(creatures.RangedMount):
+class Tank(RangedMount):
     def __init__(self):
         super(Tank, self).__init__("Tank", 0.5, 5, 6, 2, 4, 2, 4, 2)
 
-class FlyingSlime(creatures.FlyingSlimyCreature):
+class FlyingSlime(FlyingSlimyCreature):
     def __init__(self):
         super(FlyingSlime, self).__init__("Flying Slime", 0.4, 5, 4, 4, 6, -2)
 
-class PsiLord(creatures.RangedCreature):
+class PsiLord(RangedCreature):
     def __init__(self):
         super(PsiLord, self).__init__("Psi-Lord", 0.7, 3, 3, 2, 7, 0, 2, 1)
 
-class PsiWarrior(creatures.Creature):
+class PsiWarrior(Creature):
     def __init__(self):
         super(PsiWarrior, self).__init__("Psi-Warrior", 0.8, 2, 3, 1, 7, 0)
 
-class AcidSpitter(creatures.RangedCreature):
+class AcidSpitter(RangedCreature):
     def __init__(self):
         super(AcidSpitter, self).__init__("Acid Spitter", 0.6,
                                           5, 6, 1, 5, -1, 3, 3)
 
-class Mutant(creatures.Creature):
+class Mutant(Creature):
     def __init__(self):
         super(Mutant, self).__init__("Mutant", 0.6, 6, 6, 1, 5, -1)
 
-class AlienQueen(creatures.RangedCreature):
+class AlienQueen(RangedCreature):
     def __init__(self):
         super(AlienQueen, self).__init__("Alien Queen", 0.1,
                                          8, 8, 4, 5, -2, 3, 4)
 
-class MechWarrior(creatures.RangedCreature):
+class MechWarrior(RangedCreature):
     def __init__(self):
         super(MechWarrior, self).__init__("Mech Warrior", 0.1,
                                          9, 9, 3, 5, 2, 3, 4)
 
-class Tentacle(creatures.Creature):
+class Tentacle(Creature):
     def __init__(self):
         super(Tentacle, self).__init__("Tentacle", 0.5, 8, 8, 1, 6, -1)
 
-class Predator(creatures.Creature):
+class Predator(Creature):
     def __init__(self):
         super(Predator, self).__init__("Predator", 0.2, 9, 7, 5, 8, -2)
 
-class Spideroid(creatures.Creature):
+class Spideroid(Creature):
     def __init__(self):
         super(Spideroid, self).__init__("Spideroid", 0.2, 8, 8, 6, 8, 2)
 
@@ -300,7 +449,7 @@ class Symbiont(Upgrade):
         self.probability = 0.5
         self.alignment = -1
 
-def return_hand():
+def create_hand():
     all_cards = [AlienHatchling(), Probe(), Droid(), SlimeBlob(), LaserRobot(),
                  FloatingEye(), FaceGrabber(), Jeep(), SlimeWarrior(), Drone(),
                  SentryRobot(), Alien(), FireFox(), SlimeBeast(), Hovership(),

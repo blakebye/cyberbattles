1186fcc5467f2157ed58cb3b2b77741b269bbd9e
Blake Bye <blake.bye@gmail.com>
2014-09-06 23:03:26 -0700

    gives structures a commander and location

--- structures.py
+++ structures.py
@@ -7,21 +7,33 @@ class Fortress(Structure):
         self.name = "Fortress"
         self.probability = 0.6
         self.alignment = 1
+        self.x = 0
+        self.y = 0
+        self.commander = 0
 
 class GunTurret(Structure):
     def __init__(self):
         self.name = "Gun Turret"
         self.probability = 0.5
         self.alignment = 1
+        self.x = 0
+        self.y = 0
+        self.commander = 0
 
 class SubspaceBeacon(Structure):
     def __init__(self):
         self.name = "Subspace Beacon"
         self.probability = 0.9
         self.alignment = 1
+        self.x = 0
+        self.y = 0
+        self.commander = 0
 
 class ForceField(Structure):
     def __init__(self):
         self.name = "Force Field"
         self.probability = 0.9
         self.alignment = 1
+        self.x = 0
+        self.y = 0
+        self.commander = 0

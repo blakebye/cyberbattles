cf60c40c19884018a75ee884f61be16dfc635ff1
Blake Bye <blake.bye@gmail.com>
2014-09-07 05:40:59 -0700

    test play_cards for every card, some work

--- tests.py
+++ tests.py
@@ -1,7 +1,14 @@
+import random
+
 import board
-gameboard = board.Gameboard(15, 10, 5)
-gameboard.spawn_commanders()
-print gameboard.commander1
-print gameboard.commander1.hand[6].commander
+
+players = random.randint(1, 6)
+gameboard = board.Gameboard(15, 10, players)
+
+for player_number in range(players):
+    print gameboard.commanders[player_number].hand
+    for card in gameboard.commanders[player_number].hand:
+        print card.name
+        gameboard.commanders[player_number].play_card(card)
 
 gameboard.print_board()

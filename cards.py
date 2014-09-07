e71b25378a45cd7b499486b618114b289dac3d8a
Blake Bye <blake.bye@gmail.com>
2014-09-06 21:52:24 -0700

    add all creatures as a new card class

---
+++ cards.py
@@ -0,0 +1,130 @@
+import board
+import creatures
+import structures
+import spells
+
+class AlienHatchling(creatures.Creature):
+    def __init__(self):
+        super(AlienHatchling, self).__init__("Alien Hatchling", 
+                                             0.9, 2, 3, 1, 0, -2)
+
+class Probe(creatures.FlyingCreature):
+    def __init__(self):
+        super(Probe, self).__init__("Probe", 0.9, 1, 1, 4, 2, 1)
+
+class Droid(creatures.Creature):
+    def __init__(self):
+        super(Droid, self).__init__("Droid", 0.8, 3, 2, 1, 3, 1)
+
+class SlimeBlob(creatures.SlimyCreature):
+    def __init__(self):
+        super(SlimeBlob, self).__init__("Slime Blob", 0.7, 3, 2, 1, 3, -1)
+
+class LaserRobot(creatures.RangedCreature):
+    def __init__(self):
+        super(LaserRobot, self).__init__("Laser Robot", 0.6, 
+                                         5, 1, 2, 4, 2, 5, 4)
+
+class FloatingEye(creatures.FlyingCreature):
+    def __init__(self):
+        super(FloatingEye, self).__init__("Floating Eye", 0.5, 4, 3, 6, 3, -1)
+
+class FaceGrabber(creatures.Creature):
+    def __init__(self):
+        super(FaceGrabber, self).__init__("Face Grabber", 0.8, 2, 2, 3, 4, -2)
+
+class Jeep(creatures.Mount):
+    def __init__(self):
+        super(Jeep, self).__init__("Jeep", 0.9, 2, 2, 4, 4, 1)
+
+class SlimeWarrior(creatures.SlimyCreature):
+    def __init__(self):
+        super(SlimeWarrior, self).__init__("Slime Warrior", 0.6, 4, 2, 2, 4, -1)
+
+class Drone(creatures.Creature):
+    def __init__(self):
+        super(Drone, self).__init__("Drone", 0.9, 1, 3, 4, 4, 1)
+
+class SentryRobot(creatures.Creature):
+    def __init__(self):
+        super(SentryRobot, self).__init__("Sentry Robot", 0.7, 3, 2, 2, 5, 1)
+
+class Alien(creatures.Creature):
+    def __init__(self):
+        super(Alien, self).__init__("Alien", 0.7, 3, 3, 2, 5, 2)
+
+class FireFox(creatures.Mount):
+    def __init__(self):
+        super(FireFox, self).__init__("Fire Fox", 0.7, 1, 3, 3, 5, -1)
+
+class SlimeBeast(creatures.SlimyCreature):
+    def __init__(self):
+        super(SlimeBeast, self).__init__("Slime Beast", 0.5, 4, 3, 3, 5, -2)
+
+class Hovership(creatures.FlyingMount):
+    def __init__(self):
+        super(Hovership, self).__init__("Hovership", 0.6, 2, 5, 5, 4, 2)
+
+class MutantMount(creatures.Mount):
+    def __init__(self):
+        super(MutantMount, self).__init__("Mutant Mount", 0.6, 3, 4, 5, 5, -2)
+
+class Cyborg(creatures.Creature):
+    def __init__(self):
+        super(Cyborg, self).__init__("Cyborg", 0.5, 6, 6, 2, 4, 0)
+
+class HoverTank(creatures.FlyingRangedMount):
+    def __init__(self):
+        super(HoverTank, self).__init__("Hover Tank", 0.4, 5, 6, 4, 4, 2, 3, 2)
+
+class MiningDroid(creatures.RangedCreature):
+    def __init__(self):
+        super(MiningDroid, self).__init__("Mining Droid", 0.6, 
+                                          5, 6, 1, 4, 1, 2, 2)
+
+class Tank(creatures.RangedMount):
+    def __init__(self):
+        super(Tank, self).__init__("Tank", 0.5, 5, 6, 2, 4, 2, 4, 2)
+
+class FlyingSlime(creatures.FlyingSlimyCreature):
+    def __init__(self):
+        super(FlyingSlime, self).__init__("Flying Slime", 0.4, 5, 4, 4, 6, -2)
+
+class PsiLord(creatures.RangedCreature):
+    def __init__(self):
+        super(PsiLord, self).__init__("Psi-Lord", 0.7, 3, 3, 2, 7, 0, 2, 1)
+
+class PsiWarrior(creatures.Creature):
+    def __init__(self):
+        super(PsiWarrior, self).__init__("Psi-Warrior", 0.8, 2, 3, 1, 7, 0)
+
+class AcidSpitter(creatures.RangedCreature):
+    def __init__(self):
+        super(AcidSpitter, self).__init__("Acid Spitter", 0.6,
+                                          5, 6, 1, 5, -1, 3, 3)
+
+class Mutant(creatures.Creature):
+    def __init__(self):
+        super(Mutant, self).__init__("Mutant", 0.6, 6, 6, 1, 5, -1)
+
+class AlienQueen(creatures.RangedCreature):
+    def __init__(self):
+        super(AlienQueen, self).__init__("Alien Queen", 0.1,
+                                         8, 8, 4, 5, -2, 3, 4)
+
+class MechWarrior(creatures.RangedCreature):
+    def __init__(self):
+        super(MechWarrior, self).__init__("Mech Warrior", 0.1,
+                                         9, 9, 3, 5, 2, 3, 4)
+
+class Tentacle(creatures.Creature):
+    def __init__(self):
+        super(Tentacle, self).__init__("Tentacle", 0.5, 8, 8, 1, 6, -1)
+
+class Predator(creatures.Creature):
+    def __init__(self):
+        super(Predator, self).__init__("Predator", 0.2, 9, 7, 5, 8, -2)
+
+class Spideroid(creatures.Creature):
+    def __init__(self):
+        super(Spideroid, self).__init__("Spideroid", 0.2, 8, 8, 6, 8, 2)

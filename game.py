dea63aa410ca998d9bd0454a73f5c70cceea1a09
Blake Bye <blake.bye@gmail.com>
2014-09-08 04:59:40 -0700

    5 turns of card selection/targetting, works good

--- game.py
+++ game.py
@@ -2,27 +2,13 @@ import random
 import board
 import cards
 
-# players = random.randint(1, 6)
-players = 2
+players = random.randint(2, 6)
 gameboard = board.Gameboard(players)
-d = cards.Drone()
-d.commander = gameboard.commanders[1]
-d.gameboard = gameboard
-gameboard.commanders[1].creatures.append(d)
-gameboard.beam_creature(7, 7, d)
 
-while gameboard.round < 35:
-    # select card
-    for player_number in range(players):
-        player = gameboard.commanders[player_number]
-        player.play_card()
-
-    # target card, if necessary
-    for player_number in range(players):
-        pass
-
-    # move creatures
-    for player_number in range(players):
-        gameboard.commanders[player_number].move()
-
-    gameboard.next_round()
+for turn in range(5):
+    for player in range(players):
+        gameboard.commanders[player].choose_card()
+    for player in range(players):
+        gameboard.commanders[player].target_card()
+    for player in range(players):
+        gameboard.commanders[player].move()
